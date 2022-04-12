c957ee737c306a5c5842dcace2cd2806a323a4ba
little stars <89115875+First-World@users.noreply.github.com>
2022-04-12 15:36:36 +0800

    online modify

--- src/indi/czh/gittest/Gittest.java
+++ src/indi/czh/gittest/Gittest.java
@@ -7,5 +7,6 @@ public class Gittest {
         System.out.println("hello hot-fix1");
         System.out.println("hello master1");
         System.out.println("hello hot-fix2");
+        System.out.println("modify in github online");
     }
 }
